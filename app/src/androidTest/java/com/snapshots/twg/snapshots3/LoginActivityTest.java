275205095fcaea1e47240baffd3a4e3f8a391713
John Grant <johnkgrant@gmail.com>
2016-02-18 16:56:32 -0500

    Second test works

---
+++ app/src/androidTest/java/com/snapshots/twg/snapshots3/LoginActivityTest.java
@@ -0,0 +1,20 @@
+package com.snapshots.twg.snapshots3;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+/**
+ * Created by jgrant on 2016-02-18.
+ */
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
+
+    @Test
+    public void works() {
+        assertTrue(1 == 2);
+    }
+
+}
